7f9d244a5a4376d3385eb31e79914ca387e6f160
root <root@DESKTOP-5L8V8DT>
2024-11-10 19:41:25 +0800

    fix bug

--- scripts/A1.sh
+++ scripts/A1.sh
@@ -9,7 +9,7 @@ optimizer=(
 
 loss=(
     "entropy",
-    "mse"
+    "mse",
 )
 epochs=50
 batch_size=64
@@ -21,7 +21,9 @@ mkdir -p "$log_dir"
 for optim in "${optimizer[@]}"; do
     for lo in "${loss[@]}"; do
         for ((i=1; i<=max_seed; i++)); do
-            LOG_FILE = "${log_dir}/${optim}_${lo}_${seed}.log"
+            LOG_FILE="${log_dir}/${optim}_${lo}_${seed}.log"
+            echo "optimizer is $optim, loss is $lo, batch size is $batch_size"
+            echo "log will be output to $LOG_FILE"
             nohup python3 ./A1/a1.py --loss $lo --optim $optim --batch_size $batch_size \
                     --epochs $epochs --eval_interval 5 -seed $i >> $LOG_FILE 2>&1 &
         done
